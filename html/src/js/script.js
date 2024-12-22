5d1fd1db819c7160603970075f3aa1a2e5ca07c0
DESKTOP-9E21RFJ\Kreso <kresimir.bernardic@gmail.com>
2024-12-22 13:06:21 +0100

    npm postavke

---
+++ html/src/js/script.js
@@ -0,0 +1,12 @@
+import "./jquery-global.js";
+import $ from "jquery";
+
+//import '../../node_modules/jquery/dist/jquery.min.js';
+import '../../node_modules/slick-carousel/slick/slick.min.js';
+import '../../node_modules/jquery.cookie/jquery.cookie.js';
+
+$(document).ready(function() {
+
+})
+
+
