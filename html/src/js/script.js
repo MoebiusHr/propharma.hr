4a39b8dd0f801cb43bfa26f1fe1c8ab1f9ea760b
DESKTOP-9E21RFJ\Kreso <kresimir.bernardic@gmail.com>
2024-12-22 20:47:04 +0100

    radi se.

--- html/src/js/script.js
+++ html/src/js/script.js
@@ -6,15 +6,32 @@ import '../../node_modules/slick-carousel/slick/slick.min.js';
 import '../../node_modules/jquery.cookie/jquery.cookie.js';
 
 $(function() {
-    $("header .container ul li a").on("mouseenter", function() {
+
+    $("header .container nav > ul > li > a.has-children").on("mouseenter", function() {
+        $("header ul.dropdown").hide();
         $(this).next().show();
+        $(this).addClass("has-children-rotated");
     })
 
 
     $("header .container ul li ul").on("mouseleave", function() {
         $(this).hide();
+        $(this).prev().removeClass("has-children-rotated");
     })
 
+    $(document).on( 
+        'keydown', function(event) { 
+        if (event.key == "Escape") { 
+            $("header .container ul li a + ul").hide();
+        } 
+    }); 
+
+
+    $(".rotator").slick({
+        fade: true,
+        dots: true,
+        arrows: false
+    });
+    
+
 })
-
-
